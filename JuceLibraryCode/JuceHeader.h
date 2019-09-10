79c39a495ae7406a41260110c62dac4f06d7af34
ffont <frederic.font@upf.edu>
2019-09-10 15:33:28 +0200

    Add macOS exporter, rename demo app

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -37,8 +37,8 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "FreesoundAPI";
-    const char* const  companyName    = "";
+    const char* const  projectName    = "FreesoundAPI-Demo";
+    const char* const  companyName    = "Music Technology Group";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
 }
